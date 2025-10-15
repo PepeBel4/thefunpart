5c6906d407d2caeaa800d0b95a74f8762f1d21ab
PepeBel4 <pascal.peeters.0513@gmail.com>
2025-10-15 12:03:32 +0200

    Add Dutch, French, and German localisation

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,18 @@ import { provideRouter } from '@angular/router';
 import { LOCALE_ID } from '@angular/core';
 import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { registerLocaleData } from '@angular/common';
-import localeEs from '@angular/common/locales/es';
+import localeNl from '@angular/common/locales/nl';
+import localeFr from '@angular/common/locales/fr';
+import localeDe from '@angular/common/locales/de';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 import { authInterceptor } from './app/core/auth.interceptor';
 import { TranslationService } from './app/core/translation.service';
 import 'zone.js'; // Required unless you enable experimental zoneless mode
 
-registerLocaleData(localeEs);
+registerLocaleData(localeNl);
+registerLocaleData(localeFr);
+registerLocaleData(localeDe);
 
 bootstrapApplication(AppComponent, {
   providers: [
