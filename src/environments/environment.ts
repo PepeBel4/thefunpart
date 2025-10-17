5495b05795681e06d7d038c31d072a9a725a387f
PepeBel4 <pascal.peeters.0513@gmail.com>
2025-10-18 00:26:08 +0200

    Adding the public keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiBaseUrl: '/api/v1',
   // If your Rails API uses cookie sessions (Devise default), keep tokens null and rely on withCredentials
   useCookieSession: true,
-  webPushPublicKey: ''
+  webPushPublicKey: 'BGt0cT1EtNud6QYXBhsxpYDg1-Bk4gdlxGuxkebv1afK6dqtmYyc8XJwRJT6925NFwsLj9TYrEvA9j3qRCo1av4'
 };
