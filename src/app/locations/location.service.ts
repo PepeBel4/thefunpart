e03c406cd0b538a46817340f10e465bf46887e7d
PepeBel4 <pascal.peeters.0513@gmail.com>
2025-10-17 10:02:35 +0200

    Add admin controls for location opening hours

--- src/app/locations/location.service.ts
+++ src/app/locations/location.service.ts
@@ -1,6 +1,13 @@
 import { Injectable, inject } from '@angular/core';
 import { ApiService } from '../core/api.service';
-import { Location, LocationInput } from '../core/models';
+import {
+  Location,
+  LocationInput,
+  LocationOpeningHour,
+  LocationOpeningHourException,
+  LocationOpeningHourExceptionInput,
+  LocationOpeningHourInput,
+} from '../core/models';
 
 @Injectable({ providedIn: 'root' })
 export class LocationService {
@@ -33,5 +40,71 @@ export class LocationService {
 
   delete(id: number) {
     return this.api.delete<void>(`/locations/${id}`);
+  }
+
+  listOpeningHours(locationId: number) {
+    return this.api.get<LocationOpeningHour[]>(`/locations/${locationId}/location_opening_hours`);
+  }
+
+  createOpeningHour(locationId: number, payload: LocationOpeningHourInput) {
+    return this.api.post<LocationOpeningHour>(`/locations/${locationId}/location_opening_hours`, {
+      location_opening_hour: payload,
+    });
+  }
+
+  updateOpeningHour(
+    locationId: number,
+    openingHourId: number,
+    payload: LocationOpeningHourInput
+  ) {
+    return this.api.put<LocationOpeningHour>(
+      `/locations/${locationId}/location_opening_hours/${openingHourId}`,
+      {
+        location_opening_hour: payload,
+      }
+    );
+  }
+
+  deleteOpeningHour(locationId: number, openingHourId: number) {
+    return this.api.delete<void>(
+      `/locations/${locationId}/location_opening_hours/${openingHourId}`
+    );
+  }
+
+  listOpeningHourExceptions(locationId: number) {
+    return this.api.get<LocationOpeningHourException[]>(
+      `/locations/${locationId}/location_opening_hour_exceptions`
+    );
+  }
+
+  createOpeningHourException(
+    locationId: number,
+    payload: LocationOpeningHourExceptionInput
+  ) {
+    return this.api.post<LocationOpeningHourException>(
+      `/locations/${locationId}/location_opening_hour_exceptions`,
+      {
+        location_opening_hour_exception: payload,
+      }
+    );
+  }
+
+  updateOpeningHourException(
+    locationId: number,
+    exceptionId: number,
+    payload: LocationOpeningHourExceptionInput
+  ) {
+    return this.api.put<LocationOpeningHourException>(
+      `/locations/${locationId}/location_opening_hour_exceptions/${exceptionId}`,
+      {
+        location_opening_hour_exception: payload,
+      }
+    );
+  }
+
+  deleteOpeningHourException(locationId: number, exceptionId: number) {
+    return this.api.delete<void>(
+      `/locations/${locationId}/location_opening_hour_exceptions/${exceptionId}`
+    );
   }
 }
